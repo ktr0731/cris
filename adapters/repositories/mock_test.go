14e09dcd5f1d541caf753889c802ba6e7231f979
ktr0731 <aizu.s1230022@gmail.com>
2017-12-06 13:01:48 +0900

    add file repository tests

---
+++ adapters/repositories/mock_test.go
@@ -0,0 +1,40 @@
+package repositories
+
+import (
+	"testing"
+
+	"github.com/ktr0731/cris/domain/entities"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMockFileRepositoryAdapter_Store(t *testing.T) {
+	repository := NewMockFileRepository(nil, nil)
+	expected := entities.NewFile("url")
+	_, err := repository.Store(expected)
+	require.NoError(t, err)
+	v, ok := repository.storage.Load(expected.ID)
+	require.True(t, ok)
+	actual, ok := v.(*entities.File)
+	require.True(t, ok)
+	assert.Equal(t, expected, actual)
+}
+
+func TestMockFileRepositoryAdapter_Find(t *testing.T) {
+	repository := NewMockFileRepository(nil, nil)
+	expected := entities.NewFile("url")
+	_, err := repository.Store(expected)
+	require.NoError(t, err)
+	actual, err := repository.Find(expected.ID)
+	require.NoError(t, err)
+	assert.Equal(t, expected, actual)
+}
+
+func TestMockFileRepositoryAdapter_Remove(t *testing.T) {
+	repository := NewMockFileRepository(nil, nil)
+	expected := entities.NewFile("url")
+	_, err := repository.Store(expected)
+	require.NoError(t, err)
+	_, err = repository.Remove(expected.ID)
+	require.NoError(t, err)
+}
